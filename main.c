a89dca8e675a37a1d35b457ff283af93adb61b10
Geoffrey Bonneville <gbonne69@j04.script.univ-paris-diderot.fr>
2014-04-04 10:54:05 +0200

    positions de la bille

--- main.c
+++ main.c
@@ -6,12 +6,12 @@
 int main()
 {
 
-    FILE *pos_temps;
+    FILE *positions;
 
     double const PAS = 1;
 
     double vz0 = -1;
-    double z0 = 5;
+    double z0 = 50;
 
     double vx0 = 1;
     double x0 = 0;
@@ -20,7 +20,7 @@ int main()
 
     double t1, vx1, vz1, t, z1, x1;
 
-    pos_temps = fopen("pos_temps.txt", "w+");
+    positions = fopen("positions.txt", "w+");
 
     for(t=0; t<=50; t+=PAS) {
         t1 = t + PAS;
@@ -30,14 +30,14 @@ int main()
         x1 = x0 + (t1 - t) * vx0;
         vx1 =  vx0;
         printf("%6.3f\t%6.3f\t%6.3f\t%6.3f\t%6.3f\n", t, x0, vx0, z0, vz0);
-        fprintf(pos_temps, "%6.3f\t%6.3f\t%6.3f\t%6.3f\t%6.3f\n", t, x0, vx0, z0, vz0);
+        fprintf(positions, "%6.3f\t%6.3f\t%6.3f\t%6.3f\t%6.3f\n", t, x0, vx0, z0, vz0);
         z0 = z1;
         vz0 = vz1;
         x0 = x1;
         vx0 = vx1;
     }
 
-    fclose(pos_temps);
+    fclose(positions);
 
     return 0;
 }
---
+++ positions.gnuplot
@@ -0,0 +1,8 @@
+set xlabel "x (m)"
+set ylabel "z (m)"
+set title "position de la bille"
+plot "positions.txt" u 2:4
+set term postscript color
+set output "positions.ps"
+plot "positions.txt" u 2:4
+set term x11
